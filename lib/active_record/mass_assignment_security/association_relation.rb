b4b062c4ea86c134786d1ccdd2d4f8262bbe3164
Lance Vincent Bumanglag <lance@aelogica.com>
2021-01-27 19:04:30 +0800

    Fix leaky scope with first_or_create on Rails 5.2+

--- lib/active_record/mass_assignment_security/association_relation.rb
+++ lib/active_record/mass_assignment_security/association_relation.rb
@@ -1,27 +1,32 @@
-if ActiveRecord::VERSION::MAJOR >= 6
+module ActiveRecord
+  class AssociationRelation
+    undef :new
+    undef :create
+    undef :create!
 
-  module ActiveRecord
-    class AssociationRelation
-      undef :new
-      undef :create
-      undef :create!
+    def build(attributes = nil, options = {}, &block)
+      return scoping { @association.build(attributes, options, &block) } if less_than_v52
 
-      def build(attributes = nil, options = {}, &block)
-        block = _deprecated_scope_block("new", &block)
-        scoping { @association.build(attributes, options, &block) }
-      end
-      alias new build
+      @association.build(attributes, options, &block)
+    end
+    alias new build
 
-      def create(attributes = nil, options = {}, &block)
-        block = _deprecated_scope_block("create", &block)
-        scoping { @association.create(attributes, options, &block) }
-      end
+    def create(attributes = nil, options = {}, &block)
+      return scoping { @association.create(attributes, options, &block) } if less_than_v52
 
-      def create!(attributes = nil, options = {}, &block)
-        block = _deprecated_scope_block("create!", &block)
-        scoping { @association.create!(attributes, options, &block) }
-      end
+      @association.create(attributes, options, &block)
+    end
+
+    def create!(attributes = nil, options = {}, &block)
+      return scoping { @association.create!(attributes, options, &block) } if less_than_v52
+
+      @association.create!(attributes, options, &block)
+    end
+
+    private
+
+    def less_than_v52
+      ActiveRecord::VERSION::MAJOR <= 4 || (ActiveRecord::VERSION::MAJOR == 5 && ActiveRecord::VERSION::MINOR <= 1)
     end
   end
-
 end
