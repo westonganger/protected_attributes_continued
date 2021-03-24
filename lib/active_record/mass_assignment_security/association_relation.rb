b38b8a97f91d90e713ce72d3cccac7908dca530e
Andrew Vit <andrew@avit.ca>
2021-03-23 20:04:59 -0700

    Preserve scoping when building associations with conditions
    Fixes a regression from #21 where the scoping block was removed.

--- lib/active_record/mass_assignment_security/association_relation.rb
+++ lib/active_record/mass_assignment_security/association_relation.rb
@@ -5,29 +5,41 @@ module ActiveRecord
     undef :create!
 
     def build(attributes = nil, options = {}, &block)
-      if ActiveRecord::VERSION::STRING.to_f < 5.2
-        scoping { @association.build(attributes, options, &block) }
-      else
-        @association.build(attributes, options, &block)
-      end
+      block = protected_attributes_scope_block('new', block)
+      scoping { @association.build(attributes, options, &block) }
     end
     alias new build
 
     def create(attributes = nil, options = {}, &block)
-      if ActiveRecord::VERSION::STRING.to_f < 5.2
-        scoping { @association.create(attributes, options, &block) }
-      else
-        @association.create(attributes, options, &block)
-      end
+      block = protected_attributes_scope_block('create', block)
+      scoping { @association.create(attributes, options, &block) }
     end
 
     def create!(attributes = nil, options = {}, &block)
-      if ActiveRecord::VERSION::STRING.to_f < 5.2
-        scoping { @association.create!(attributes, options, &block) }
-      else
-        @association.create!(attributes, options, &block)
-      end
+      block = protected_attributes_scope_block('create!', block)
+      scoping { @association.create!(attributes, options, &block) }
     end
 
+    private
+
+    if ActiveRecord.gem_version < Gem::Version.new('6.0')
+
+      def protected_attributes_scope_block(_label, block)
+        block
+      end
+
+    elsif ActiveRecord.gem_version < Gem::Version.new('6.1')
+
+      def protected_attributes_scope_block(label, block)
+        _deprecated_scope_block(label, &block)
+      end
+
+    else
+
+      def protected_attributes_scope_block(_label, block)
+        current_scope_restoring_block(&block)
+      end
+
+    end
   end
 end
