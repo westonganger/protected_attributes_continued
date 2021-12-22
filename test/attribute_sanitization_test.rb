368123b5712752d8073523aaf9e2c406075c2f7b
Weston Ganger <weston@westonganger.com>
2021-12-22 11:06:48 -0800

    Update Test Suite

--- test/attribute_sanitization_test.rb
+++ test/attribute_sanitization_test.rb
@@ -253,9 +253,12 @@ class AttributeSanitizationTest < ActiveSupport::TestCase
 
   def test_protection_against_class_attribute_writers
     attribute_writers = [:logger, :configurations, :primary_key_prefix_type, :table_name_prefix, :table_name_suffix, :pluralize_table_names,
-     :default_timezone, :schema_format, :lock_optimistically, :timestamped_migrations, :default_scopes,
-     :connection_handler, :nested_attributes_options,
+     :lock_optimistically, :default_scopes, :connection_handler, :nested_attributes_options,
      :attribute_method_matchers, :time_zone_aware_attributes, :skip_time_zone_conversion_for_attributes]
+
+    if Rails::VERSION::MAJOR <= 6
+      attribute_writers += [:default_timezone, :schema_format, :timestamped_migrations]
+    end
 
     attribute_writers.push(:_attr_readonly) if ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR == 0
 
